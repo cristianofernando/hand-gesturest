4f31c1ce9259fbbb2b007dc9f7ab805c649cd69e
cristianofernando <110285936+cristianofernando@users.noreply.github.com>
2024-02-09 19:18:09 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+//https://teachablemachine.withgoogle.com/models/UGnbXuHyZ/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:100
+});
+
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tQEf5kfNt/model.json',modelLoaded);
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediciton_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function gotresult(error,results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        
+    }
+}
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotresult)
+}
